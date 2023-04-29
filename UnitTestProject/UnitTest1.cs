eb66d564230b7d8ba3ac67bbd05ac2253ab87edc
КасаткинаЕИ <KasatkinaLiza03@mail.ru>
2023-04-29 13:59:18 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Work_for_unit_test;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, Program.work_with_file());
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(Program.MethodTrue());
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.IsFalse(Program.MethodFalse());
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.IsNotNull(Program.MethodFalse());
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.IsNull(Program.MethodNull());
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.IsNotNull(Program.work_with_file());
+        }
+
+        //[TestMethod]
+        //public void TestMethod7()
+        //{
+        //    Assert.ThrowsException(Program.MethodArray());
+        //}
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Assert.AreNotEqual(3,Program.work_with_file());
+        }
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Assert.IsInstanceOfType(Program.work_with_file(), typeof(int));
+        }
+        public void TestMethod10()
+        {
+            Assert.IsNotInstanceOfType(Program.work_with_file(), typeof(double));
+        }
+
+    }
+}
---
+++ Work_for_unit_test/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_for_unit_test
+{
+    public class Program
+    {
+        public static int work_with_file()
+        {
+            int x = 1 + 1;
+            return x;
+        }
+        public static Boolean MethodTrue()
+        {
+            return true;
+        }
+        public static Boolean MethodFalse()
+        {
+            return false;
+        }
+
+        public static object MethodNull()
+        {
+            object a = null;
+            return a;
+        }
+
+        public static int[] MethodArray()
+        {
+            int[] arr = new int[3];
+            for (int i = 0; i < 4; i++)
+            {
+                arr[i] = i;
+            }
+            return arr;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
